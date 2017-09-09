527aa0918ad512cd56e70016189dba7e0a35eda3
Johan Kämpe <kampe.johan@gmail.com>
2017-09-09 17:48:04 +0200

    Use external file to store ssid/pw. Minor changes to webpage

--- wifi_toggle_led/wifi_toggle_led.ino
+++ wifi_toggle_led/wifi_toggle_led.ino
@@ -1,13 +1,10 @@
 #include <ESP8266WiFi.h>
 #include <ArduinoNodeMCU.h>
+#include <WiFiSettings.h> // SSID & Password
 
 #define RED_LED_PIN PIN_GPIO16
 #define BLUE_LED_PIN PIN_GPIO5
 #define STRING_NOT_FOUND -1
-
-/* WiFi info */
-//const char * ssid = "YourSSID";
-//const char * password = "YourPasword";
 
 String toggle_red = "/TOGGLE-RED";
 String toggle_blue = "/TOGGLE-BLUE";
@@ -16,6 +13,7 @@ bool red_status;
 bool blue_status;
 
 WiFiServer server(80);
+WiFiClient client;
 
 void setup()
 {
@@ -32,7 +30,7 @@ void setup()
     red_status = false;
 
     /* Connect to WiFi */
-    WiFi.begin(ssid, password);
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
     Serial.print("Connecting");
 
     while (WiFi.status() != WL_CONNECTED)
@@ -53,7 +51,7 @@ void setup()
 void loop()
 {
     /* Check for new client connected to server */
-    WiFiClient client = server.available();
+    client = server.available();
 
     if (!client)
     {
@@ -80,25 +78,38 @@ void loop()
     digitalWrite(RED_LED_PIN, red_status ? HIGH : LOW);
     digitalWrite(BLUE_LED_PIN, blue_status ? HIGH : LOW);
 
-    // Return the response
-    client.println("HTTP/1.1 200 OK");
-    client.println("Content-Type: text/html");
-    client.println(""); //  do not forget this one
-    client.println("<!DOCTYPE HTML>");
-    client.println("<html>");
-
-    client.print("RED: ");
-    client.println(red_status ? "ON" : "OFF");
-    client.print("BLUE: ");
-    client.println(blue_status ? "ON" : "OFF");
-
-    client.println("<br><br>");
-    client.println("<a href=\"" + toggle_red + "\"\"><button>Toggle RED</button></a>");
-    client.println("<a href=\"" + toggle_blue + "\"\"><button>Toggle BLUE</button></a><br />");
-    client.println("</html>");
+    clientResponse(); // Show webpage
 
     delay(1);
     Serial.println("Client disonnected");
     Serial.println("");
+}
 
+void clientResponse()
+{
+    client.println("HTTP/1.1 200 OK");
+    client.println("Content-Type: text/html");
+    client.println("");
+    client.println("<!DOCTYPE HTML>");
+
+    /* HTML */
+    client.println("<html>");
+    client.println("<title>LED TOGGLER!</title>");
+    client.println("<h1>LED TOGGLER!</h1>");
+
+    /* Status */
+    client.print("<br>RED: ");
+    client.print(red_status ? "ON" : "OFF");
+    client.print("<br>BLUE: ");
+    client.print(blue_status ? "ON" : "OFF");
+
+    client.println("<br><br>");
+
+    /* Buttons for toggling LEDs */
+    client.println("<a href=\"" + toggle_red +
+        "\"\"><button>Toggle RED</button></a>");
+    client.println("<a href=\"" + toggle_blue +
+        "\"\"><button>Toggle BLUE</button></a><br />");
+
+    client.println("</html>");
 }
