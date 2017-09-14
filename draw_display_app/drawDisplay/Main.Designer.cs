9a926fb8138c9fdccab835ac06ce87b59d0eed80
Johan Kämpe <kampe.johan@gmail.com>
2017-09-14 19:18:05 +0200

    Draw App for draw_display NodeMCU program

---
+++ draw_display_app/drawDisplay/Main.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace drawDisplay
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelDrawArea = new System.Windows.Forms.Panel();
+            this.lblCursorX = new System.Windows.Forms.Label();
+            this.lblCursorY = new System.Windows.Forms.Label();
+            this.textBoxDrawSize = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // panelDrawArea
+            // 
+            this.panelDrawArea.Location = new System.Drawing.Point(76, 97);
+            this.panelDrawArea.Name = "panelDrawArea";
+            this.panelDrawArea.Size = new System.Drawing.Size(128, 64);
+            this.panelDrawArea.TabIndex = 0;
+            this.panelDrawArea.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panelDrawArea_MouseDown);
+            this.panelDrawArea.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panelDrawArea_MouseMove);
+            this.panelDrawArea.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panelDrawArea_MouseUp);
+            // 
+            // lblCursorX
+            // 
+            this.lblCursorX.AutoSize = true;
+            this.lblCursorX.Location = new System.Drawing.Point(76, 178);
+            this.lblCursorX.Name = "lblCursorX";
+            this.lblCursorX.Size = new System.Drawing.Size(35, 13);
+            this.lblCursorX.TabIndex = 1;
+            this.lblCursorX.Text = "label1";
+            // 
+            // lblCursorY
+            // 
+            this.lblCursorY.AutoSize = true;
+            this.lblCursorY.Location = new System.Drawing.Point(169, 178);
+            this.lblCursorY.Name = "lblCursorY";
+            this.lblCursorY.Size = new System.Drawing.Size(35, 13);
+            this.lblCursorY.TabIndex = 2;
+            this.lblCursorY.Text = "label2";
+            // 
+            // textBoxDrawSize
+            // 
+            this.textBoxDrawSize.Location = new System.Drawing.Point(76, 216);
+            this.textBoxDrawSize.Name = "textBoxDrawSize";
+            this.textBoxDrawSize.Size = new System.Drawing.Size(100, 20);
+            this.textBoxDrawSize.TabIndex = 3;
+            this.textBoxDrawSize.Text = "1";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.textBoxDrawSize);
+            this.Controls.Add(this.lblCursorY);
+            this.Controls.Add(this.lblCursorX);
+            this.Controls.Add(this.panelDrawArea);
+            this.Name = "frmMain";
+            this.Text = "drawDisplay APP";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelDrawArea;
+        private System.Windows.Forms.Label lblCursorX;
+        private System.Windows.Forms.Label lblCursorY;
+        private System.Windows.Forms.TextBox textBoxDrawSize;
+    }
+}
+
---
+++ draw_display_app/drawDisplay/Main.cs
@@ -0,0 +1,75 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace drawDisplay
+{
+    public partial class frmMain : Form
+    {
+        int displayWidth = 128;
+        int displayHeight = 64;
+
+        Graphics graphics;
+        Brush brush = Brushes.White;
+
+        bool draw;
+
+        public frmMain()
+        {
+            InitializeComponent();
+            SetPanel();
+
+            graphics = panelDrawArea.CreateGraphics();
+
+            lblCursorX.Text = "X:";
+            lblCursorY.Text = "Y:";
+
+            draw = false;
+        }
+
+
+
+        #region panel
+
+        /* Init panel */
+        void SetPanel()
+        {
+            panelDrawArea.Height = displayHeight;
+            panelDrawArea.Width = displayWidth;
+            panelDrawArea.BackColor = Color.Black;
+        }
+
+        /* Mouse moves over panel area */
+        private void panelDrawArea_MouseMove(
+            object sender, MouseEventArgs e)
+        {
+            lblCursorX.Text = "X: " + e.Location.X.ToString();
+            lblCursorY.Text = "Y: " + e.Location.Y.ToString();
+
+            if (draw)
+            {
+                graphics.FillRectangle(
+                    brush, 
+                    e.Location.X, 
+                    e.Location.Y, 
+                    int.Parse(textBoxDrawSize.Text), 
+                    int.Parse(textBoxDrawSize.Text)
+               );
+            }
+        }
+
+        /* Mouse button down, over panel area */
+        private void panelDrawArea_MouseDown(
+            object sender, MouseEventArgs e)
+        {
+            draw = true;
+        }
+
+        /* Mouse button up, over panel area */
+        private void panelDrawArea_MouseUp(object sender, MouseEventArgs e)
+        {
+            draw = false;
+        }
+
+        #endregion
+    }
+}
