8ef517a555439040f128403c91c2ef90df9fbca1
Johan Kämpe <kampe.johan@gmail.com>
2017-09-21 11:49:26 +0200

    Animate colors

--- dotstar/dotstar.ino
+++ dotstar/dotstar.ino
@@ -3,9 +3,9 @@
 #include <ArduinoNodeMCU.h>
 
 /*   Led-strip   */
-#define STRIP_NUM_LEDS 10
-#define NODEMCU_PIN_DATA D5
-#define NODEMCU_PIN_CLOCK D8
+#define STRIP_NUM_LEDS 50
+#define NODEMCU_PIN_DATA D5 // 4
+#define NODEMCU_PIN_CLOCK D8 //5
 #define STRIP_BRIGHTNESS_MAX 250
 
 /*   Init LED-strip as "strip"  */
@@ -13,19 +13,35 @@ Adafruit_DotStar strip = Adafruit_DotStar(
     STRIP_NUM_LEDS,
     NODEMCU_PIN_DATA,
     NODEMCU_PIN_CLOCK,
-    DOTSTAR_RGB
+    DOTSTAR_BRG
   );
 
 void setup()
 {
     strip.begin();
-    strip.setPixelColor(1, 0xFF0000);
-    strip.setPixelColor(2, 0xFF0000);
-    strip.setPixelColor(3, 0xFF0000);
-    strip.show();
+    strip.setBrightness(250);
 }
 
 void loop()
 {
-    delay(1);
+    for(int i = 0; i < STRIP_NUM_LEDS; i++)
+    {
+        strip.setPixelColor(i, 0,0,255);
+        strip.show();
+        delay(50);
+    }
+
+    for(int i = 0; i < STRIP_NUM_LEDS; i++)
+    {
+        strip.setPixelColor(i, 0,255,0);
+        strip.show();
+        delay(50);
+    }
+
+    for(int i = 0; i < STRIP_NUM_LEDS; i++)
+    {
+        strip.setPixelColor(i, 255,0,0);
+        strip.show();
+        delay(50);
+    }
 }
